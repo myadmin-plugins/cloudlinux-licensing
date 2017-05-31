d72971931c3959b2730f2667a757a5aad4e9e233
Joe Huss <detain@interserver.net>
2017-05-30 21:14:16 -0400

    kernelcare scripts

---
+++ scripts/lookup_kcare.php
@@ -0,0 +1,32 @@
+<?php
+require_once (__DIR__ . '/../../../include/functions.inc.php');
+$url="https://cln.cloudlinux.com/clweb/xmlrpc";
+$license_type=16;
+///home/my/files/CloudLinux/php/clnreg.kcare-check.php
+$cl_login=CLOUDLINUX_LOGIN;
+$secret_key=CLOUDLINUX_KEY;
+$IP=$argv[1];
+$now=time();
+$sha1hash=sha1("$secret_key$now");
+$auth_token="$cl_login|$now|$sha1hash";
+
+$BODY="<?xml version=\"1.0\" encoding=\"UTF-8\"?> <methodCall> <methodName>registration.is_licensed</methodName> <params> <param> <value>$auth_token</value> </param> <param> <value>$IP</value> </param> <param><value><boolean>1</boolean></value></param></params> </methodCall>";
+$context = stream_context_create
+	(array(
+	 'http' => array(
+			 'method' => 'POST',
+			 'header' => 'Content-Type: application/xml; charset=UTF-8',
+			 'content' => $BODY
+			 )
+	 ));
+
+$result = file_get_contents($url, false, $context);
+if (mb_strpos($result, '<i4>0</i4>')) {
+	echo "Not Licensed\n";
+} elseif (mb_strpos($result, '<i4>16</i4>')) {
+	echo "KernelCare License\n";
+} elseif (mb_strpos($result, '<i4>1</i4>')) {
+	echo "License Type 1\n";
+} elseif (mb_strpos($result, '<i4>2</i4>')) {
+	echo "License Type 2\n";
+}
