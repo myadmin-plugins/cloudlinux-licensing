6badd08e0dcc70365211687a460592523b54857c
Joe Huss <detain@interserver.net>
2017-06-16 08:13:56 -0400

    updates to the plugin migrating the varios data in the myadmin.php file into the Plugin class itself so i can then phase out the myadmin.php file and then a developer only needs to setup this one simple file to setup a new plugin.

--- src/Plugin.php
+++ src/Plugin.php
@@ -8,7 +8,27 @@ use MyAdmin\Settings;
 
 class Plugin {
 
+	public static $name = 'Cloudlinux Licensing';
+	public static $description = 'Allows selling of Cloudlinux Server and VPS License Types.  More info at https://www.cloudlinux.com/';
+	public static $help = 'It provides more than one million end users the ability to quickly install dozens of the leading open source content management systems into their web space.  	Must have a pre-existing cPanel license with cPanelDirect to purchase a cloudlinux license. Allow 10 minutes for activation.';
+	public static $module = 'licenses';
+	public static $type = 'service';
+
+
 	public function __construct() {
+	}
+
+	public static function Hooks() {
+		return [
+			'plugin.install' => ['Detain\MyAdminCloudlinux\Plugin', 'Install'],
+			'plugin.uninstall' => ['Detain\MyAdminCloudlinux\Plugin', 'Uninstall'],
+			'licenses.settings' => ['Detain\MyAdminCloudlinux\Plugin', 'Settings'],
+			'licenses.activate' => ['Detain\MyAdminCloudlinux\Plugin', 'Activate'],
+			'licenses.deactivate' => ['Detain\MyAdminCloudlinux\Plugin', 'Deactivate'],
+			'licenses.change_ip' => ['Detain\MyAdminCloudlinux\Plugin', 'ChangeIp'],
+			'function.requirements' => ['Detain\MyAdminCloudlinux\Plugin', 'Requirements'],
+			'ui.menu' => ['Detain\MyAdminCloudlinux\Plugin', 'Menu'],
+		];
 	}
 
 	public static function Install(GenericEvent $event) {
