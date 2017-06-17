76cbb3541a937c6d9c9f3cae62b5f7a88717a5a2
Joe Huss <detain@interserver.net>
2017-06-16 22:44:55 -0400

    updates

--- bin/cloudlinux_check.php
+++ bin/cloudlinux_check.php
@@ -4,6 +4,4 @@ use Detain\Cloudlinux\Cloudlinux;
 require_once(__DIR__ . '/../../../include/functions.inc.php');
 
 $cl = new Cloudlinux(CLOUDLINUX_LOGIN, CLOUDLINUX_KEY);
-//print_r($cl->check($_SERVER['argv'][1]));
-//print_r($cl->xmlIsLicensed($_SERVER['argv'][1]));
 print_r($cl->isLicensed($_SERVER['argv'][1]));
