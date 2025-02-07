ba440d54f61080a1cfa81be200fd12853341c81d
Joe Huss <detain@interserver.net>
2025-02-07 16:43:28 -0500

    added cli scripts for 2 of the new api commands

---
+++ bin/cloudlinux_types.php
@@ -0,0 +1,14 @@
+<?php
+
+use Detain\Cloudlinux\Cloudlinux;
+
+ini_set('display_errors', 'on');
+require_once __DIR__.'/../../../../include/functions.inc.php';
+require_once __DIR__.'/../../cloudlinux-licensing/src/Cloudlinux.php';
+$webpage = false;
+define('VERBOSE_MODE', false);
+global $console;
+
+$cl = new Cloudlinux(CLOUDLINUX_LOGIN, CLOUDLINUX_KEY);
+$types = $cl->types();
+print_r($types);
